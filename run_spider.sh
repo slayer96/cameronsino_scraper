e51fe66ae8d95e8a813ca4dd0411b5868a7d77c3
Mykhailo Mykytyn <mykhailo.m@zazmic.com>
2018-11-29 16:05:48 +0200

    add remove file every 30 days

--- run_spider.sh
+++ run_spider.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+find /home/ubf19/cameronsino_data/ -maxdepth 1 -mtime +30 -type f -delete
 cd /home/ubf19/cameronsino_scraper/cameronsino_scraper
 PATH=$PATH:/usr/local/bin
 export PATH
